de2dd9d80da9b7be81b2e39730726d89d5911bca
LeoPincle <leo.soto965@gmail.com>
2026-03-13 14:26:11 -0600

    update networks stack for eks

--- stacks/network/main.tf
+++ stacks/network/main.tf
@@ -2,6 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# VPC
 resource "aws_vpc" "platform" {
   cidr_block = "10.0.0.0/16"
 
@@ -10,19 +11,38 @@ resource "aws_vpc" "platform" {
   }
 }
 
-resource "aws_subnet" "public" {
-  vpc_id     = aws_vpc.platform.id
-  cidr_block = "10.0.1.0/24"
+# Subnet AZ1
+resource "aws_subnet" "public_a" {
+  vpc_id                  = aws_vpc.platform.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
 
   tags = {
-    Name = "platform-subnet"
+    Name = "platform-subnet-a"
   }
 }
 
+# Subnet AZ2
+resource "aws_subnet" "public_b" {
+  vpc_id                  = aws_vpc.platform.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "platform-subnet-b"
+  }
+}
+
+# Outputs
 output "vpc_id" {
   value = aws_vpc.platform.id
 }
 
-output "subnet_id" {
-  value = aws_subnet.public.id
+output "subnet_ids" {
+  value = [
+    aws_subnet.public_a.id,
+    aws_subnet.public_b.id
+  ]
 }
