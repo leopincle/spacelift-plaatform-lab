40afb40566047aa4a59ae529850be8ee2794bb9c
LeoPincle <leo.soto965@gmail.com>
2026-03-13 15:15:18 -0600

    fixing tf files

--- stacks/eks/main.tf
+++ stacks/eks/main.tf
@@ -3,7 +3,10 @@ provider "aws" {
 }
 
 variable "vpc_id" {}
-variable "subnet_ids" {}
+
+variable "subnet_ids" {
+  type = any
+}
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
@@ -12,8 +15,10 @@ module "eks" {
   cluster_name    = "spacelift-eks"
   cluster_version = "1.29"
 
-  vpc_id = var.vpc_id
-  subnet_ids = var.subnet_ids
+  vpc_id     = var.vpc_id
+  subnet_ids = tolist(var.subnet_ids)
+
+  enable_cluster_creator_admin_permissions = true
 
   eks_managed_node_groups = {
     default = {
--- stacks/network/main.tf
+++ stacks/network/main.tf
@@ -2,31 +2,45 @@ provider "aws" {
   region = "us-east-1"
 }
 
-variable "vpc_id" {}
+resource "aws_vpc" "platform" {
+  cidr_block = "10.0.0.0/16"
 
-variable "subnet_ids" {
-  type = any
+  tags = {
+    Name = "platform-vpc"
+  }
 }
 
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20.0"
+resource "aws_subnet" "public_a" {
+  vpc_id            = aws_vpc.platform.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
 
-  cluster_name    = "spacelift-eks"
-  cluster_version = "1.29"
+  map_public_ip_on_launch = true
 
-  vpc_id     = var.vpc_id
-  subnet_ids = tolist(var.subnet_ids)
-
-  enable_cluster_creator_admin_permissions = true
-
-  eks_managed_node_groups = {
-    default = {
-      desired_size = 1
-      max_size     = 1
-      min_size     = 1
-
-      instance_types = ["t3.small"]
-    }
+  tags = {
+    Name = "platform-subnet-a"
   }
+}
+
+resource "aws_subnet" "public_b" {
+  vpc_id            = aws_vpc.platform.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "platform-subnet-b"
+  }
+}
+
+output "vpc_id" {
+  value = aws_vpc.platform.id
+}
+
+output "subnet_ids" {
+  value = [
+    aws_subnet.public_a.id,
+    aws_subnet.public_b.id
+  ]
 }
